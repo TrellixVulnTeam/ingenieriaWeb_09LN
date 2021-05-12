8295a2c62ae25ed522daa0f3e9f84eb60a428a45
System Administrator <root@MacBook-Pro-de-sandra.local>
2021-05-12 12:37:54 -0400

    creando servicio web con NodeJS usando DB mysql

--- WebService/api.ts
+++ WebService/api.ts
@@ -1,9 +1,15 @@
 const express=require('express');
 const mysql=require('mysql');
 const app=express();
+const bodyParser = require('body-parser');
 
- const servidor="127.0.0.1";
- const puerto=3007;
+ //const servidor="127.0.0.1";
+ //const puerto=3007;
+
+ const Configuracion={
+     server:"127.0.0.1",
+     port : 3016
+ };
 
  //configuracion de la conexión
  let connection = mysql.createConnection({
@@ -23,12 +29,15 @@ const app=express();
        //aqui debemos colocar lo demás 
   });
 
+//app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.json())
+
   //métodos CRUD=Create ==post, Read==get, Update==put, Delete==delete
  app.get('/',(req:any,res:any)=>{
      res.send("hola mundo");
  });
 
-app.get('/usuarios',(req:any,res:any)=>{
+app.get('/usuarios',bodyParser.json(),(req:any,res:any)=>{
        connection.query("SELECT * FROM usuarios",(req1:any,resultados:any)=>{
            console.log(resultados);
            //res.send(resultados);
@@ -44,13 +53,53 @@ app.get('/usuarios/:id',(req:any,res:any)=>{
     });
 });
 
-app.post('',(req:any,res:any)=>{
-    
+//insertar [nombre ,correoelectronico,clave]
+app.post('/crearUsuarios',(req:any,res:any)=>{
+     let nombre=req.body.nombre;
+     let correoeletronico=req.body.correoelectronico;
+     let clave=req.body.clave;
+
+
+     console.log(nombre);
+
+     connection.query("INSERT INTO usuarios(nombre,correo_electronico,contrasena)VALUES('"+nombre+"','"+correoeletronico+"','"+clave+"')",(req1:any,resultados:any)=>{
+            
+        res.status(201).send(`Usuario creado con el id:${resultados.insertId}`);
+        //console.log(resultados);
+     });
 });
 
+/*
+app.put('/modificarusuario/:idUsuario',(req:any,res:any)=>{
+     let id=req.params.idUsuario;
+     let nombre=req.body.nombre;
+     
+     connection.query("UPDATE usuarios SET nombre=? WHERE id=?",[nombre,id],(req1:any,resultados:any)=>{
+         res.status(200).send("OK actualizado");
+     });
+});
+*/
 
- app.listen(puerto,()=>{
-     console.log(`servidor escuchando localhost:${puerto}`);
+app.put('/modificarusuario',(req:any,res:any)=>{
+    let id=req.body.idUsuario;
+    let nombre=req.body.nombre;
+
+    connection.query("UPDATE usuarios SET nombre=? WHERE id=?",[nombre,id],(req1:any,resultados:any)=>{
+        res.status(200).send("OK actualizado");
+    });
+});
+
+app.delete('/borrar/:id',(req:any,res:any)=>{
+    let id=req.params.id;
+    connection.query('DELETE FROM usuarios WHERE id=?',id,(res1:any,resultados:any)=>{
+     res.status(200).send("dato eliminado");
+    });
+})
+
+
+
+ app.listen(Configuracion,()=>{
+     console.log(`servidor escuchando ${Configuracion.server}:${Configuracion.port}`);
  });
 
 
--- WebService/js/api.js
+++ WebService/js/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
-var servidor = "127.0.0.1";
-var puerto = 3007;
+var bodyParser = require('body-parser');
+//const servidor="127.0.0.1";
+//const puerto=3007;
+var Configuracion = {
+    server: "127.0.0.1",
+    port: 3016
+};
 //configuracion de la conexión
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -19,11 +24,13 @@ connection.connect(function (error) {
     console.log('conectado a mysql');
     //aqui debemos colocar lo demás 
 });
+//app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.json())
 //métodos CRUD=Create ==post, Read==get, Update==put, Delete==delete
 app.get('/', function (req, res) {
     res.send("hola mundo");
 });
-app.get('/usuarios', function (req, res) {
+app.get('/usuarios', bodyParser.json(), function (req, res) {
     connection.query("SELECT * FROM usuarios", function (req1, resultados) {
         console.log(resultados);
         //res.send(resultados);
@@ -37,6 +44,40 @@ app.get('/usuarios/:id', function (req, res) {
         res.status(200).send(resultados);
     });
 });
-app.listen(puerto, function () {
-    console.log("servidor escuchando localhost:" + puerto);
+//insertar [nombre ,correoelectronico,clave]
+app.post('/crearUsuarios', function (req, res) {
+    var nombre = req.body.nombre;
+    var correoeletronico = req.body.correoelectronico;
+    var clave = req.body.clave;
+    console.log(nombre);
+    connection.query("INSERT INTO usuarios(nombre,correo_electronico,contrasena)VALUES('" + nombre + "','" + correoeletronico + "','" + clave + "')", function (req1, resultados) {
+        res.status(201).send("Usuario creado con el id:" + resultados.insertId);
+        //console.log(resultados);
+    });
+});
+/*
+app.put('/modificarusuario/:idUsuario',(req:any,res:any)=>{
+     let id=req.params.idUsuario;
+     let nombre=req.body.nombre;
+     
+     connection.query("UPDATE usuarios SET nombre=? WHERE id=?",[nombre,id],(req1:any,resultados:any)=>{
+         res.status(200).send("OK actualizado");
+     });
+});
+*/
+app.put('/modificarusuario', function (req, res) {
+    var id = req.body.idUsuario;
+    var nombre = req.body.nombre;
+    connection.query("UPDATE usuarios SET nombre=? WHERE id=?", [nombre, id], function (req1, resultados) {
+        res.status(200).send("OK actualizado");
+    });
+});
+app.delete('/borrar/:id', function (req, res) {
+    var id = req.params.id;
+    connection.query('DELETE FROM usuarios WHERE id=?', id, function (res1, resultados) {
+        res.status(200).send("dato eliminado");
+    });
+});
+app.listen(Configuracion, function () {
+    console.log("servidor escuchando " + Configuracion.server + ":" + Configuracion.port);
 });
