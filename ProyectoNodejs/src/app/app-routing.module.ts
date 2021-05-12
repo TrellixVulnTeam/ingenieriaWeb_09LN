aa29a40d7670df308e350008675ea5d1f9325c0b
System Administrator <root@MacBook-Pro-de-sandra.local>
2021-05-12 09:19:29 -0400

    Proyecto en nodeJS (Backend) y AngularCLI(Frontend)

---
+++ ProyectoNodejs/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {UsuariosComponent} from '../app/usuarios/usuarios.component';
+
+const routes: Routes = [
+  {path:'',component:UsuariosComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
