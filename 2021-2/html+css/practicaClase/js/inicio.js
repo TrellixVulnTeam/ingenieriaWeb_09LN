b4300a5334dedfda9ae30880230c03558f92688e
sancano22 <sandra.cano@gmail.com>
2021-08-18 11:27:39 -0400

    actualizacion de JS

---
+++ 2021-2/html+css/practicaClase/js/inicio.js
@@ -0,0 +1,18 @@
+/*var elemento=["1.png","2.png","3.png","4.png"];
+
+
+for(var i=0;i<elemento.length;i++){
+	 console.log("elemento de "+ elemento[i]);
+}*/
+
+var elemento=document.getElementById("galeria");
+
+elemento.addEventListener("click",function(){
+	console.log("hola mundo");
+});
+
+
+
+console.log(elemento);
+
+
