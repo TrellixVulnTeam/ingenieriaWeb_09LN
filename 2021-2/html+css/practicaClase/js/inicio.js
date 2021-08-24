758901480c2c1427d8a1166cc5d2022342fa4040
sancano22 <sandra.cano@gmail.com>
2021-08-24 09:57:16 -0400

    Galeria dinámica con JS

--- 2021-2/html+css/practicaClase/js/inicio.js
+++ 2021-2/html+css/practicaClase/js/inicio.js
@@ -1,18 +1,64 @@
-/*var elemento=["1.png","2.png","3.png","4.png"];
+var imagenes=["1.png","2.png","3.png","4.png","5.png","6.png","7.png"];
+
+window.addEventListener("load",cargar);
+
+function cargar(){
+var fotos=document.getElementById("fotos");
+
+for (var i=0;i<imagenes.length;i++){
+
+	var elemento=document.createElement("li");
+	var elemento2=document.createElement("img");
+	var elemento3=document.createElement("div");
+	var elemento4=document.createElement("span");
+	var elemento5=document.createElement("span");
+	elemento2.src="imgs/"+imagenes[i];
+	elemento2.alt="descripcion "+imagenes[i];
+	elemento4.className="material-icons red";
+
+	elemento5.id="fuente";
+
+	fotos.appendChild(elemento);
+	elemento.appendChild(elemento2);
+	elemento.appendChild(elemento3);
+	elemento3.appendChild(elemento4).textContent="favorite";
+    elemento3.appendChild(elemento5).textContent=1;
+
+     elemento5.addEventListener("click",function(){
+	      console.log("Hola");
+     });
+}
+}
 
 
+
+
+
+/*
+var elementoEvento=document.getElementById("fuente");
+elementoEvento.addEventListener("click",function(){
+	 console.log("Hola");
+});
+*/
+
+
+/*
 for(var i=0;i<elemento.length;i++){
 	 console.log("elemento de "+ elemento[i]);
-}*/
+}
 
 var elemento=document.getElementById("galeria");
 
-elemento.addEventListener("click",function(){
+elemento.addEventListener("click",evento);
+
+function evento(){
+	
 	console.log("hola mundo");
-});
 
-
+}
 
 console.log(elemento);
+
+*/
 
 
